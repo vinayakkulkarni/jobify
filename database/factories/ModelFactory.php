86375fa4b72332ce081ad18388fe33c7d183280d
Vinayak Kulkarni <vinayak@shoptreeapp.com>
2017-08-01 02:23:32 +0530

    [Database] Seeders

--- database/factories/ModelFactory.php
+++ database/factories/ModelFactory.php
@@ -22,3 +22,17 @@ $factory->define(App\User::class, function (Faker\Generator $faker) {
         'remember_token' => str_random(10),
     ];
 });
+
+$factory->define(App\Job::class, function (Faker\Generator $faker) {
+  return [
+    'reference' => $faker->domainWord,
+    'employer' => $faker->company,
+    'title' => $faker->jobTitle,
+    'location' => $faker->city,
+    'salary' => $faker->numberBetween($min = 1000, $max = 9000),
+    'post_date' => $faker->dateTimeBetween($startDate = '-30 years',
+        $endDate = 'now', $timezone = date_default_timezone_get()),
+    'type' => $faker->jobTitle,
+    'description' => $faker->sentence
+  ];
+});
--- database/seeds/JobSeeder.php
+++ database/seeds/JobSeeder.php
@@ -2,7 +2,7 @@
 
 use Illuminate\Database\Seeder;
 
-class DatabaseSeeder extends Seeder
+class JobSeeder extends Seeder
 {
     /**
      * Run the database seeds.
@@ -11,7 +11,7 @@ class DatabaseSeeder extends Seeder
      */
     public function run()
     {
-        $this->call(UserSeeder::class);
-        $this->call(JobSeeder::class);
+        DB::table('jobs')->delete();
+        factory(App\Job::class, 12)->create();
     }
 }
